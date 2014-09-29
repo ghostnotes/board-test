3c5aea44ff4a2509bc1b836d91f6f0823026ede2
ghostnotes <ghostnotesdot@gmail.com>
2014-09-30 00:06:09 +0900

    Job categories has been added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Job.delete_all
+
+100.times do |i|
+  Job.create!(
+    title: i % 2 == 0 ? 'User Interface Designer' : 'User experience design specialist',
+    category: i % 2 == 0 ? :in_house : :entrusted,
+    headquarters: 'Japan',
+    workplace: i % 2 == 0 ? 'Shibuya' : 'Shinjuku',
+    description: "The origin of the word pizza is uncertain and debated. One popular suggestion holds that it originates from the Greek pita (derived from ancient Greek pēktos, πηκτός, meaning 'solid' or 'clotted'[11]). The ancient Greeks covered their bread with oils, herbs and cheese. The Romans developed placenta, a sheet of dough topped with cheese and honey and flavored with bay leaves. A popular contemporary legend holds that the archetypal pizza, Pizza Margherita, was invented in 1889, when the Royal Palace of Capodimonte commissioned the Neapolitan pizzaiolo (pizza maker) Raffaele Esposito to create a pizza in honor of the visiting Queen Margherita. Of the three different pizzas he created, the Queen strongly preferred a pie swathed in the colors of the Italian flag: red (tomato), green (basil), and white (mozzarella). Supposedly, this kind of pizza was then named after the Queen as 'Pizza Margherita',[12] although recent research casts doubt on this legend.[13] Pizza began being served in the United States with the arrival of Italian migrants, and the first pizzeria in the country, Lombardi's Pizza, opened in 1905.[14] After World War II many returning soldiers who were stationed in Italy created a high demand for the pizza they encountered and tasted in Italy. Since then pizza has evolved many variations. It can be deep-dish pizza, stuffed pizza, pizza pockets, pizza turnovers, rolled pizza, pizza-on-a-stick, all with combinations of sauce and toppings limited only by one's inventiveness.",
+    how_to_apply: 'send an email.',
+    company_name: 'Test' + i.to_s + ', Inc.',
+    company_email: 'test' + i.to_s + '@test.test',
+    company_url: 'http://test.test/' + i.to_s,
+    company_logo_url: 'http://test.test/' + i.to_s + '/logo'
+  )
+end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -3,18 +3,26 @@ require 'rails_helper'
 
 describe Job do
 
-  describe '' do
-    context '' do
+  describe '#new' do
+    context '正常' do
       before do
         expect(Job.count).to eq 0
-        Job.create!(title: 'Test Job Title', category: Job.categories[:in_house])
+        Job.categories.each do |k, v|
+          Job.create!(title: 'Test Job Title' + k.to_s, category: Job.categories[k])
+        end
       end
 
       it do
-        job = Job.first
-        puts job.to_json
-        puts job.in_house?.to_s
-        puts job.entrusted?.to_s
+        expect(Job.count).to eq Job.categories.length
+        jobs = Job.limit(2)
+        expect(jobs[0].in_house?).to eq true
+        expect(jobs[1].entrusted?).to eq true
+      end
+    end
+
+    context '存在しないカテゴリの場合' do
+      it do
+        expect{ Job.create!(category: 999) }.to raise_error(ArgumentError)
       end
     end
   end
